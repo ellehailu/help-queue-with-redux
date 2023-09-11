4b1f98e2f8be4a5d13be49e76602e8060b5f127f
ellehailu <hailu.elle@gmail.com>
2023-09-11 15:03:10 -0700

    add redux to ticketControl

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -2,29 +2,30 @@ import ticketListReducer from '../../reducers/ticket-list-reducer';
 
 describe('ticketListReducer', () => {
 
+  let action;
+
   const currentState = {
     1: {
       names: 'Ryan & Aimen',
       location: '4b',
       issue: 'Redux action is not working correctly.',
-      id: 1
-    },
-    2: {
+      id: 1 
+    }, 2: {
       names: 'Jasmine and Justine',
       location: '2a',
       issue: 'Reducer has side effects.',
-      id: 2
+      id: 2 
     }
   }
-  let action;
+
   const ticketData = {
-    names:'Ryan & Aimen',
+    names: 'Ryan & Aimen',
     location: '4b',
     issue: 'Redux action is not working correctly.',
     id: 1
   };
 
-  test('Should return default state if there is no action type passed into the reducer', () => {
+  test('Should return default state if no action type is recognized', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
 
@@ -37,31 +38,29 @@ describe('ticketListReducer', () => {
       issue: issue,
       id: id
     };
-
-  expect(ticketListReducer({}, action)).toEqual({
-    [id]: {
-      names: names,
-      location: location,
-      issue: issue,
-      id: id
-    }
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
   });
-});
 
   test('Should successfully delete a ticket', () => {
     action = {
       type: 'DELETE_TICKET',
       id: 1
     };
-    expect (ticketListReducer(currentState, action)).toEqual({
+    expect(ticketListReducer(currentState, action)).toEqual({
       2: {
         names: 'Jasmine and Justine',
         location: '2a',
         issue: 'Reducer has side effects.',
-        id: 2
+        id: 2 
       }
     });
   });
 
-  
 });
