2622bbb904bc6ec0d290f2d0956823172ce360a5
ellehailu <hailu.elle@gmail.com>
2023-09-11 10:44:28 -0700

    passed second test

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -9,21 +9,19 @@ describe('ticketListReducer', () => {
     id: 1
   };
 
-
-// eslint-disable-next-line jest/no-identical-title
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
 
-test('Should successfully add new ticket data to mainTicketList', () => {
-  const { names, location, issue, id } = ticketData;
-  action = {
-    type: 'ADD_TICKET',
-    names: names,
-    location: location,
-    issue: issue,
-    id: id
-  };
+  test('Should successfully add new ticket data to mainTicketList', () => {
+    const { names, location, issue, id } = ticketData;
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      id: id
+    };
 
   expect(ticketListReducer({}, action)).toEqual({
     [id]: {
